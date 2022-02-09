2a0c3b392c45bfc9cf3f7e8c5ff87801a3dfdef0
alvaroormeno <alvaro@ormeno.org>
2022-02-09 17:50:12 -0500

    fixed robot name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -57,7 +57,6 @@ var fight = function(enemy) {
 
 var startGame = function() {
 
-    // reset player stats
 
     // reset player stats
     playerInfo.reset();
@@ -68,6 +67,7 @@ var startGame = function() {
         if (playerInfo.health > 0){
             // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            //debugger;
 
             // pick new enemy to fight based on the index of the enemyNames array
             var pickedEnemyObj = enemyInfo[i];
@@ -164,8 +164,20 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+  
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+  
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robots name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
