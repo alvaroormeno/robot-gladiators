8efc2873fb8a0444fbb36548cd69a5800772453b
alvaroormeno <alvaro@ormeno.org>
2022-02-10 15:27:52 -0500

    added high score function to game

--- assets/js/game.js
+++ assets/js/game.js
@@ -156,11 +156,21 @@ var startGame = function() {
 
 // function to end the entire game
 var endGame = function() {
-    //if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("The game has now ended. Lets see how you did!")
+    window.alert("The game has now ended. Lets see how you did!");
+
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highScore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + " !")
     } else {
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
 
     // ask player if they'd like to play again
@@ -185,13 +195,13 @@ var shop = function () {
 
     // use switch to carry out action
     switch (shopOptionPrompt) {
-        case "1":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "2":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "3":
+        case 3:
             window.alert("Leaving the store.");
             // do nothing so the function will end
             break;
